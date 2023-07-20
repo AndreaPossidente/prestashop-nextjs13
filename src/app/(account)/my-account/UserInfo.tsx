be9a5dbf25a4973d6f580b0cba0b358cbd68a9b6
Andrea Possidente <ndrpssdnt@gmail.com>
2023-07-20 10:20:25 +0200

    move next-auth from pages to app folder

--- src/app/(account)/my-account/UserInfo.tsx
+++ src/app/(account)/my-account/UserInfo.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Customer } from "@/Prestashop/models";
 import { getServerSession } from "next-auth/next";
 import { Suspense } from "react";
